5a0bb182dc11ba5f2cc513946e647a5be78930c9
Antonio Rioseco <antoniok.rioseco@gmail.com>
2024-03-05 19:51:28 +0000

    Removed all points in asia, greenland is still there.

--- src/Mapping.R
+++ src/Mapping.R
@@ -23,4 +23,23 @@ map <- leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
 
 mapshot2(map, file = "output/occurrenceMap.png")
 
+map
+
 #The occurrence points are in great numbers on the west and east coast of the U.S., and start to decrease as you move towards the middle of the U.S.
+
+#trying to remove non north america points 
+
+maptwo <- leaflet(options = leafletOptions(zoomControl = FALSE)) %>% 
+  addProviderTiles("Esri.WorldTopoMap") %>%
+  addCircleMarkers(data = testFilter,
+                   lat = ~decimalLatitude,
+                   lng = ~decimalLongitude,
+                   radius = 3,
+                   color = "hotpink",
+                   fillOpacity = 0.8) %>%
+  addLegend(position = "topright",
+            title= "species occurence from gbif", 
+            labels ="Myotis Lucifugus", 
+            colors = "hotpink")
+
+maptwo
--- src/Mlucifugis.R
+++ src/Mlucifugis.R
@@ -69,6 +69,9 @@ batdata <- batdata %>%
 
 write.csv(batdata, "data/cleanedData.csv")
 
+#trying to remove all non north america points 
+testFilter = data %>%
+  subset(decimalLongitude<0)
 
 
 
